ab8c13a4463ff04f2e275510cb5c82fee41eb393
Egor <zhora.levchenkoko@gmail.com>
2024-06-04 13:02:36 +0300

    Додав опис перечисленням

--- Core/Enums/TestAttemptStatus.cs
+++ Core/Enums/TestAttemptStatus.cs
@@ -1,10 +1,17 @@
-﻿namespace Core.Enums;
+﻿using System.ComponentModel;
+
+namespace Core.Enums;
 
 public enum TestAttemptStatus
 {
+    [Description("В процесі")]
     InProcess = 0,
+    [Description("Завершено")]
     Completed = 1,
+    [Description("Прострочено")]
     Expired = 2,
+    [Description("Завалено")]
     Failed = 3,
+    [Description("Відмінений")]
     Cancelled = 4,
 }
